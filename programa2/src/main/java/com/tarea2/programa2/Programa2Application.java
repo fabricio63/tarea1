f89e2c76689706636dd5324db30bbc6e6ba72909
fabricio_ufm <fabriciojuarez@ufm.edu>
2020-01-13 08:03:31 -0600

    tarea 2

---
+++ programa2/src/main/java/com/tarea2/programa2/Programa2Application.java
@@ -0,0 +1,13 @@
+package com.tarea2.programa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Programa2Application.class, args);
+	}
+
+}
---
+++ programa2/src/test/java/com/tarea2/programa2/Programa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2.programa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
